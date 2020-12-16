65dbcf66ff2aaeb439e955817827372def293bbe
Benedikt Gräler <b.graeler@52north.org>
2020-12-16 21:32:43 +0100

    fix typos in comments

--- demo/MRP.R
+++ demo/MRP.R
@@ -9,7 +9,7 @@ plot(peakVol, asp=1)
 # Kendall's tau correlation
 cor(triples,method="kendall")
 
-# estiamte the BB7 copula by means of maximum likelihood
+# estimate the BB7 copula by means of maximum likelihood
 copQV <- fitCopula(BB7Copula(param=c(2,14)), peakVol)@copula
 copQV
 
@@ -21,7 +21,7 @@ v_MAR <- c(0.9, invdduCopula(0.9, copQV, 0.9))
 v_MAR
 pCopula(v_MAR, copQV)
 
-## the anlytical kendall distribution
+## the analytical kendall distribution
 kendallFunQV <- getKendallDistr(copQV)
 
 # the Kendall distribution value (fraction of pairs having a smaller copula value than "t")
@@ -33,7 +33,7 @@ curve(kendallFunQV, from=0, to=1, asp=1)
 t_KEN2 <- criticalLevel(kendallFunQV, 10, mu=1)
 t_KEN2
 
-# the corresponmding KEN2-RP for the OR-RP
+# the corresponding KEN2-RP for the OR-RP
 kendallRP(kendallFunQV, cl=0.9, mu=1, copula=copQV)
 
 # illustrating the critical lines (empirically)
